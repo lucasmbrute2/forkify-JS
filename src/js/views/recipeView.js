fc51eb4ae79633babdc206e349043120f15fe510
Lucas <lucasmbrute614@gmail.com>
2022-04-29 09:44:42 -0300

    Changing fraction lib

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from 'fractional-arithmetic';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
